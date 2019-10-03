8c7de674af156ac2f4d32a742960a9c79eb25570
laiso <laiso@lai.so>
2019-10-03 23:54:59 +0700

    GKE cluster

---
+++ main.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+variable "project" {
+  default = "todo-app"
+}
+
+variable "service_account_json" {
+  default = "secrets/service_account.json"
+}
+
+provider "google" {
+  project    = "${var.project}-${terraform.workspace}"
+  region     = "${var.region}"
+  zone       = "${var.zone}"
+  credential = "${file("${var.service_account_json}")}"
+}
+
+resource "google_container_cluster" "primary" {
+  project            = "${var.project}-${terraform.workspace} "
+  name               = "${var.project}"
+  location           = "${var.zone}"
+  initial_node_count = 1
+}
